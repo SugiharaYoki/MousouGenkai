62f76e9b83ddc6419848816cadf5c8ca189f5e2c
SugiharaYoki <yukidare2766@gmail.com>
2023-05-23 01:27:39 -0700

    Added more PopMimi possibilities

--- popmimi.js
+++ popmimi.js
@@ -4,13 +4,28 @@ let poppedOut = false;
 
 function shouldPopOut() {
   const rng = Math.random();
-  return rng < 0.03;
+  return rng < 0.02;
 }
 
 function popOut() {
   if (poppedOut) return;
   mimi.style.transition = 'transform 3s';
-  mimi.style.transform = 'translateX(-45%)';
+  const rng2 = Math.random();
+  if (rng2 <= 0.2) {
+    mimi.style.transform = 'translateX(-45%) translateY(-45%)';
+  }
+  if (rng2 > 0.2 && rng2 <= 0.4) {
+    mimi.style.transform = 'translateX(-45%) translateY(-30%)';
+  }
+  if (rng2 > 0.4 && rng2 <= 0.6) {
+    mimi.style.transform = 'translateX(-45%) translateY(-15%)';
+  }
+  if (rng2 > 0.6 && rng2 <= 0.8) {
+    mimi.style.transform = 'translateX(-45%) translateY(-0%)';
+  }
+  if (rng2 > 0.8) {
+    mimi.style.transform = 'translateX(-45%) translateY(-15%)';
+  }
   poppedOut = true;
 }
 
