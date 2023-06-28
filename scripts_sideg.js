af1e752b6a7ea1cac1dd7bb93a0a82ce484e30e0
杉原夜季 <yukidare2766@gmail.com>
2023-06-28 23:11:49 +0800

    Added Story Side G

---
+++ scripts_sideg.js
@@ -0,0 +1,214 @@
+let currentPage = 1, chaptersPerPage = 5;
+
+const chapters = [
+  {
+    title: "光玥壹章：弑猎轮生者之策",
+    shortTitle: "光玥壹章 故事",
+    filePath: "csideg_1.txt",
+    id: 1,
+    summary: "<span class='syslang'>&nbsp&nbsp作者：竹取月&nbsp&nbsp</span><br><br>此处为剑与魔法的世界。<br>但表象以外，现状远不止如此。<br><br>任何存在玩家间竞争手段的游戏内，都会存在无休止而缘由看似滑稽的斗争乃至厮杀。<br>而竹取月，便是始终存活于这一游戏大环境中的技术型玩家。<br>只不过……月的工作，可不止是“介入斗争”而已。<br><br>只要报酬到位，不管何种任务目标都将尽全力去达成。<br>不论委托者是NPC还是其他的玩家。哪怕是杀死同族的其他玩家，她也将完成使命。<br><br>这便是月身为雇佣兵的生存守则。",
+  },
+];
+const characterAvailability = {
+  1: [5],
+};
+const linktails = [
+  {
+    content: "也来浏览一下这些网站吧！",
+    id: 1,
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const chapterList = document.getElementById("chapter-list"), chapterContent = document.getElementById("chapter-content"), tailLinks = document.getElementById("tail-links"), totalPages = Math.ceil(chapters.length / chaptersPerPage);
+
+  async function displayChapterList() {
+    chapterList.innerHTML = "";
+    for (let i = (currentPage - 1) * chaptersPerPage; i < currentPage * chaptersPerPage && i < chapters.length; i++) {
+      if (i != 4 || getCurrentPageIndex() == 4) {
+        const listItem = document.createElement("li");
+        listItem.classList.add("chapter-item");
+        const titleElement = document.createElement("div");
+        titleElement.textContent = chapters[i].title;
+        const listLinker = document.createElement("a");
+        listLinker.href = `mousou_side1_${i + 1}.html`;
+        listItem.appendChild(listLinker);
+        listLinker.appendChild(titleElement);
+        listLinker.classList.add("chapter-link");
+        const characterCount = await getCharacterCount(chapters[i].filePath);
+        const characterCountElement = document.createElement("div"), readingTime = Math.round(characterCount / 600);
+        if (i == 4) {
+          characterCountElement.textContent = `无规范时长 | ${Math.round(characterCount / 100)/100} 万字 `;
+        } else {
+        characterCountElement.textContent = `${readingTime} 分钟 | ${Math.round(characterCount / 100)/100} 万字 `;
+        }
+        characterCountElement.classList.add("chapter-character-count");
+        listLinker.appendChild(characterCountElement);
+        chapterList.appendChild(listItem);
+      }
+    }
+    displayPagination(); setActiveChapterTitle();
+  } 
+
+  function displayPagination() {
+    const pagination = document.createElement("div");
+    pagination.className = "pagination";
+
+    for (let i = 1; i <= totalPages; i++) {
+      const pageNumber = document.createElement("button");
+      pageNumber.textContent = i;
+      pageNumber.className = "page-number";
+      pageNumber.addEventListener("click", () => {
+        currentPage = i;
+        displayChapterList();
+      });
+
+      if (i === currentPage) {
+        pageNumber.classList.add("active");
+      }
+
+      pagination.appendChild(pageNumber);
+    }
+
+    chapterList.appendChild(pagination);
+  }
+  displayChapterList();
+
+  async function displayChapter(index) {
+    try {
+      const response = await fetch(chapters[index].filePath);
+      const content = await response.text();
+      chapterContent.innerHTML = `<p>${content}</p>`;
+      setCharacterAvailability(index + 1);
+      displayChapterSummary(index);
+    } catch (error) {
+      chapterContent.innerHTML = `<br><br><h3>“纵使此世由纷争与妄想所交叠纠缠，<br>汝之心念终将凝成赴往真实的微光。”</h3><br><br>`;
+      setCharacterAvailability(0);
+      displayChapterSummary(0);
+    }
+    displayTailLinks(0);
+  }
+
+  function displayTailLinks(index) {
+    tailLinks.innerHTML = `<p>${linktails[index].content}</p>`;
+  }
+
+  function setCharacterAvailability(chapterId) {
+    const availableCharacters = characterAvailability[chapterId] || [];
+    const tabSelectors = document.querySelectorAll(".tab-selector");
+    const characterLabels = document.querySelectorAll(".character-tab");
+    const termLabels = document.querySelectorAll(".term-tab");
+  
+    tabSelectors.forEach((selector, index) => {
+      selector.disabled = !availableCharacters.includes(index + 1);
+    });
+  
+    characterLabels.forEach((label, index) => {
+      if (!availableCharacters.includes(index + 1)) {
+        label.classList.add("disabled");
+      } else {
+        label.classList.remove("disabled");
+      }
+    });
+    termLabels.forEach((label, index) => {
+      label.classList.remove("disabled");
+    });
+  }
+
+  function setActiveChapterTitle() {
+    const chapterListItems = document.querySelectorAll("#chapter-list li");
+    const currentPageIndex = getCurrentPageIndex();
+    chapterListItems.forEach((item, i) => {
+      if (i === currentPageIndex) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
+  }
+  function getCurrentPageIndex() {
+    const currentPath = window.location.pathname;
+    const chapterIndexRegex = /mousou_chapter(\d+)\.html/;
+    const match = currentPath.match(chapterIndexRegex);
+  
+    if (match) {
+      return parseInt(match[1]) - 1;
+    } else {
+      return 0;
+    }
+  }
+
+  document.getElementById("toggleCharacterList").addEventListener("click", function () {
+    const characterList = document.getElementById("character-tabs");
+    page = 0;
+    const termList = document.getElementById("term-tabs");
+    const termList2 = document.getElementById("term-tabs2");
+    const termList3 = document.getElementById("term-tabs3");
+    characterList.classList.toggle("hidden");
+    termList.classList.add("hidden");
+    termList2.classList.add("hidden");
+    termList3.classList.add("hidden");
+  });
+  document.getElementById("toggleTermList").addEventListener("click", function () {
+    const termList = document.getElementById("term-tabs");
+    const termList2 = document.getElementById("term-tabs2");
+    const termList3 = document.getElementById("term-tabs3");
+    const characterList = document.getElementById("character-tabs");
+    characterList.classList.add("hidden");
+    if (!(page >= 0)) {
+      page = 0;
+    }
+    if (page == 2 && termList2 == null) {
+      page = 0;
+    }
+    if (page == 3 && termList3 == null) {
+      page = 0;
+    }
+    page += 1;
+    if (page == 1) {
+      termList.classList.remove("hidden");
+      termList2.classList.add("hidden");
+      termList3.classList.add("hidden");
+    }
+    if (page == 2) {
+      termList.classList.add("hidden");
+      termList2.classList.remove("hidden");
+      termList3.classList.add("hidden");
+    }
+    if (page == 3) {
+      termList.classList.add("hidden");
+      termList2.classList.add("hidden");
+      termList3.classList.remove("hidden");
+    }
+  });
+  document.getElementById("toggleWorldMap").addEventListener("click", function () {
+    const worldMap = document.getElementById("world-map");
+    worldMap.classList.toggle("hidden");
+  });
+
+  async function getCharacterCount(filePath) {
+    try {
+      const response = await fetch(filePath);
+      const content = await response.text();
+      return content.length;
+    } catch (error) {
+      console.error("Error fetching chapter content for character count:", error);
+      return 0;
+    }
+  }
+  function displayChapterSummary(index) {
+    const summaryBox = document.getElementById("chapter-summary");
+    summaryBox.innerHTML = `<h3>${chapters[index].shortTitle}概览</h3><p>${chapters[index].summary}</p>`;
+  }
+  currentPage = 1;
+  for (let i = 1; i <= getCurrentPageIndex(); i++) {
+    if (i / 5 === 1) {
+      currentPage += 1;
+    }
+  }
+  displayChapter(getCurrentPageIndex()); 
+  setActiveChapterTitle();
+  
+});
+
+
