463cf78198fcaae75efdd50dbe7a97b8abd61be2
SugiharaYoki <80352861+SugiharaYoki@users.noreply.github.com>
2023-07-24 11:33:50 +0800

    Cookie for userIsAbove15

--- scripts_index.js
+++ scripts_index.js
@@ -9,8 +9,31 @@ window.addEventListener('load', () => {
 
 
 function confirmAge() {
-  const answer = confirm("本小说存在不适合青少年阅读的段落，你是否已年满15岁？");
-  if (answer) {
+  const userIsAbove15 = getCookie("userIsAbove15");
+  if (userIsAbove15 != "yes") {
+    const answer = confirm("本小说存在不适合青少年阅读的段落，你是否已年满15岁？");
+    if (answer) {
+      window.location.href = "mousou_chapter0.html";
+      const isYes = true;
+      setCookie("userIsAbove15", isYes ? "yes" : "no", 365);
+    }
+  } else {
     window.location.href = "mousou_chapter0.html";
   }
+}
+
+function setCookie(name, value, days) {
+  let expires = '';
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+function getCookie(name) {
+  const value = '; ' + document.cookie;
+  const parts = value.split('; ' + name + '=');
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
